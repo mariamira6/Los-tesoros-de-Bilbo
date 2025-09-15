760b50318d0bffe10c2328526e06c7ab9d87c3ac
mariamira6 <maria.alaves93@gmail.com>
2025-09-15 14:31:49 +0200

    Implementado multilenguaje con i18n

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module ProyectoRuby
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    #available languages
+    config.i18n.available_locales = [:en, :es]
+
+    #language by default
+    config.i18n.default_locale = :es
   end
 end
